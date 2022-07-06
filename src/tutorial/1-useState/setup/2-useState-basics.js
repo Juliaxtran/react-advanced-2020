c5d891d630aae303c7ba39208c3b76d78cd9c4b0
Julia Tran <juliatran@Julias-MacBook-Air.local>
2022-07-06 14:57:53 -0400

    Use state basics

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,25 @@
 import React, { useState } from 'react';
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  // const value = useState(1)[0];
+  // const handler = useState(1)[1];
+  // console.log(value, handler);
+  const [text, setText ] = useState('random Title');
+  const handleClick = () => {
+    if (text === 'random Title') {
+    setText('Hello World');
+  } else {
+    setText('random Title');
+  }
+}
+  return (
+    <>
+    <h1>{text}</h1>
+    <button type='button' className='btn' onClick={handleClick}>
+      Change Title
+      </button>
+    </>
+  )
 };
 
 export default UseStateBasics;
