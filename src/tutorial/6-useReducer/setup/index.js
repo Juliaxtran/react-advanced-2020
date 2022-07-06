e55a04122170ac540afa7e3dded61b22b190a547
Julia Tran <juliatran@Julias-MacBook-Air.local>
2022-07-06 17:59:52 -0400

    Use Reducer

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -2,9 +2,44 @@ import React, { useState, useReducer } from 'react';
 import Modal from './Modal';
 import { data } from '../../../data';
 // reducer function
+const reducer = (state, action) => {
+  switch (action.type) {
+  }
+}
 
+const defaultState = {
+  people:[],
+  isModalOpen: false,
+  modalContent: 'hello world',
+}
 const Index = () => {
-  return <h2>useReducer</h2>;
+const [state, dispatch] = useReducer(reducer, defaultState);
+  const [showModal, setShowModal] = useState(false);
+  const [name, setName] = useState('');
+
+  const handleSubmit = (e) => {
+   e.preventDefault();
+   if(name) {
+
+   }else {
+
+   }
+  }
+
+  return <>
+    {state.isModalOpen && <Modal />}
+    <form onSubmit={handleSubmit} className='form'>
+      <div>
+        <input type='text'
+          value={name}
+          onChange ={(e) => setName(e.target.value)}/>
+
+      </div>
+      <button  className = 'btn' type='submit'>Submit</button>
+    </form>
+  {state.people.map(person => <h4 key={person.id}>{person.name}</h4>)}
+
+  </>
 };
 
 export default Index;
