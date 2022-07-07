3a291aa820ad98bbb5f8040bf37d21c04acd2c1b
Julia Tran <juliatran@Julias-MacBook-Air.local>
2022-07-07 15:43:46 -0400

    Reducer practice tutorial

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -21,8 +21,24 @@ const reducer = (state, action) => {
         modalContent: 'please enter value'
       };
   }
+  if (action.type === 'CLOSE_MODAL') {
+    return {
+      ...state,
+      isModalOpen: false,
+      modalContent: 'please enter value'
+    };
+}
+if (action.type === 'REMOVE_ITEM') {
+  const newPeople = state.people.filter(
+    (person) => person.id !== action.payload
+  );
+  return { ...state, people: newPeople };
+}
+
   throw new Error ('no matching action type')
 };
+
+
 
 // from reducer you want to reduce some sort of state
 const defaultState = {
@@ -47,8 +63,12 @@ const Index = () => {
     }
   }
 
+  const closeModal = () => {
+      dispatch({type:  'CLOSE_MODAL'});
+  }
+
   return <>
-    {state.isModalOpen && <Modal modalContent={state.modalContent} />}
+    {state.isModalOpen && <Modal closeModal = {closeModal} modalContent={state.modalContent} />}
     <form onSubmit={handleSubmit} className='form'>
       <div>
         <input type='text'
@@ -58,7 +78,16 @@ const Index = () => {
       </div>
       <button className='btn' type='submit'>Submit</button>
     </form>
-    {state.people.map(person => <h4 key={person.id}>{person.name}</h4>)}
+    {state.people.map(person => {
+      return (
+        <div key={person.id} className='item'>
+          <h4>{person.name}</h4>
+          <button className='btn' onClick={() => dispatch({type:'REMOVE_ITEM', payload:person.id})}>Delete</button>
+          </div>
+      )
+    }
+  )}
+
 
   </>
 };
